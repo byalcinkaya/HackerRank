7e54cc2985758f80d3c5217732e7479d32b3aec7
Büşra Yalçınkaya <102999054+byalcinkaya@users.noreply.github.com>
2022-12-01 14:48:55 +0300

    Add files via upload

---
+++ python-if-else.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 != 0:
+    print("Weird")
+elif (2 <= n <= 6):
+    print("Not Weird")
+elif (6 < n < 21):
+    print("Weird")
+elif (20 < n):
+    print("Not Weird")
